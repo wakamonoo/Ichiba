789d266d9bbb912658d086913f0a49423b987f3b
wakamonoo <joven.serdanbataller21@gmail.com>
2025-07-17 21:29:30 +0800

    try

--- src/productDisplay.js
+++ src/productDisplay.js
@@ -56,3 +56,27 @@ function displayProducts(products) {
 }
 
 document.addEventListener("DOMContentLoaded", fetchProducts);
+
+
+// ──────────────────────────────────────────loginModal
+// ──────────────────────────────────────────openodal
+const modal = document.querySelector("#loginModal");
+
+document.querySelector("#openModal").addEventListener("click", () => {
+  modal.classList.remove("hidden");
+  modal.classList.add("flex");
+});
+
+// ──────────────────────────────────────────closeModal
+document.querySelector("#closeModal").addEventListener("click", () => {
+  modal.classList.add("hidden");
+  modal.classList.remove("flex");
+});
+
+modal.addEventListener("click", (e) => {
+  if (e.target === modal) {
+    modal.classList.add("hidden");
+    modal.classList.remove("flex");
+  }
+});
+
